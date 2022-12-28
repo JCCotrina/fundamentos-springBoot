972669f40260a0a75d25901c9d0ea46ff98f6b45
JCCotrina <juan.cotrina.m@uni.pe>
2022-12-28 13:39:06 -0500

    creacion de una dependencia propia

--- src/main/java/com/fundamentos/springboot/fundamentos/FundamentosApplication.java
+++ src/main/java/com/fundamentos/springboot/fundamentos/FundamentosApplication.java
@@ -1,6 +1,8 @@
 package com.fundamentos.springboot.fundamentos;
 
+import com.fundamentos.springboot.fundamentos.bean.MyBean;
 import com.fundamentos.springboot.fundamentos.component.ComponentDependency;
+import com.fundamentos.springboot.fundamentos.configuration.MyConfigurationBean;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -10,9 +12,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FundamentosApplication implements CommandLineRunner {
 
 	private ComponentDependency componentDependency;
+	private MyBean myBean;
 
-	public FundamentosApplication(@Qualifier("componentTwoImplement") ComponentDependency componentDependecy){
+	public FundamentosApplication(@Qualifier("componentTwoImplement") ComponentDependency componentDependecy,
+								  MyBean myBean){
 		this.componentDependency = componentDependecy;
+		this.myBean = myBean;
 	}
 
 	public static void main(String[] args) {
@@ -22,5 +27,6 @@ public class FundamentosApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		componentDependency.saludar();
+		myBean.print();
 	}
 }
---
+++ src/main/java/com/fundamentos/springboot/fundamentos/configuration/MyConfigurationBean.java
@@ -0,0 +1,14 @@
+package com.fundamentos.springboot.fundamentos.configuration;
+
+import com.fundamentos.springboot.fundamentos.bean.MyBean;
+import com.fundamentos.springboot.fundamentos.bean.MyBean2Implement;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MyConfigurationBean {
+    @Bean
+    public MyBean beanOperation(){
+        return new MyBean2Implement();
+    }
+}
